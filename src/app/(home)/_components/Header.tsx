abcc63438e1362d7fc7a28dba4846783c6fc1d74
Nahyun <tarea202001@gmail.com>
2024-08-15 19:55:04 +0900

    Feat: 헤더 퍼블리싱 완료

--- src/app/(home)/_components/Header.tsx
+++ src/app/(home)/_components/Header.tsx
@@ -2,8 +2,63 @@
  * 홈, 검색 아이콘, 검색창, 로그인/로그아웃 버튼(비회원), 벨/유저 아이콘(회원)
  */
 
+import { useState } from 'react';
+
+import SearchBarComponent from '@/components/SearchBar';
+
+import * as styles from './Header.css';
+import SearchIcon from '/public/icons/ver3/search.svg';
+import BellIcon from '/public/icons/ver3/bell.svg';
+import Avatar from '/public/icons/ver3/avatar.svg';
+
 function Header() {
-  return <div></div>;
+  const [isSearchBarOpened, setIsSearchBarOpened] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleAuthButtonClick = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleInactivateSearchBar = () => {
+    setIsSearchBarOpened(false);
+  };
+
+  const handleSearchIconClick = () => {
+    setIsSearchBarOpened(true);
+  };
+
+  return (
+    <div className={styles.headerWrapper}>
+      {isSearchBarOpened && <SearchBarComponent handleCancel={handleInactivateSearchBar} />}
+      {!isSearchBarOpened && (
+        <div className={styles.entireWrapper}>
+          <div className={styles.homeTitleContainer}>
+            <h1 className={styles.homeTitle}>홈</h1>
+            {!isLoggedIn && (
+              <button className={styles.authButton} onClick={handleAuthButtonClick}>
+                로그인 / 로그아웃
+              </button>
+            )}
+          </div>
+          <div className={styles.iconWrapper}>
+            <button onClick={handleSearchIconClick}>
+              <SearchIcon />
+            </button>
+            {isLoggedIn && (
+              <div className={styles.iconWrapperForMember}>
+                <button>
+                  <BellIcon />
+                </button>
+                <button>
+                  <Avatar />
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Header;
