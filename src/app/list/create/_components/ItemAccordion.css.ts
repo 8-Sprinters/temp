9641c89e1dde069c611bfe5556d17ca0465feb0a
Seoyoung <seoyoungiyo@gmail.com>
2025-01-15 16:55:29 +0900

    Fix: step2 순위뱃지, 타이틀인풋 UI 깨짐 수정

--- src/app/list/create/_components/ItemAccordion.css.ts
+++ src/app/list/create/_components/ItemAccordion.css.ts
@@ -20,30 +20,34 @@ export const header = style({
   color: vars.color.red,
 });
 
-export const rank = style([
+const rankBadge = style([
   fonts.Label,
   {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    width: '4.2rem',
+    minWidth: '4.2rem',
     height: '2.6rem',
 
     color: vars.color.blue,
     backgroundColor: vars.color.lightblue,
     borderRadius: '1.5rem',
+
+    whiteSpace: 'nowrap',
   },
 ]);
 
-export const variantRank = styleVariants({
-  default: [rank],
-  first: [rank, { color: vars.color.white, backgroundColor: vars.color.blue }],
+export const variantRankBadge = styleVariants({
+  default: [rankBadge],
+  first: [rankBadge, { color: vars.color.white, backgroundColor: vars.color.blue }],
 });
 
 export const titleInput = style([
   fonts.BodyBold,
   {
-    flexGrow: 1,
+    flexGrow: 1, //남는 공간 차지하게
+    minWidth: '0', //부모컨테이너에 맞춰 줄어들 수 있도록 강제 (안넣을 경우 내용에 맞춰서 크기 유지)
+
     color: vars.color.bluegray10,
     '::placeholder': { color: vars.color.bluegray6 },
   },
@@ -57,6 +61,7 @@ export const accordionIconWrapper = style({
 
   width: '2rem',
   height: '2.6rem',
+  flexShrink: 0, //아이콘은 줄어들지 않도록 설정
 });
 
 //콘텐트
